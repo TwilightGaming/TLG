ea71139fd4e99028f74b5041c7fad6f26f5e202b
TwilightGaming <144401914+Dkt-Github@users.noreply.github.com>
2023-09-24 17:31:18 +0900

    Add files via upload

---
+++ TLGweb/scripts/scripts.js
@@ -0,0 +1,21 @@
+console.log('javascript loaded');
+$(function() {
+    $('html,body').animate({ scrollTop: 0 }, '1');
+});
+$(window).on('load',function() {
+    $("#splash").delay(1300).fadeOut('slow',function(){
+});
+});
+$(function () {
+    $(window).scroll(function(){
+      var windowHeight = $(window).height(),
+      scrollY = $(window).scrollTop();
+      $('.third-area, .valo-text-wrapper, .valo-div-img, .ssbu-text-wrapper, .ssbu-div-img').each(function(){
+        var thisPosition = $(this).offset().top;
+        if(scrollY > thisPosition - windowHeight){
+          $(this).addClass('scrollin');
+        }
+      });
+    });
+  });
+let submitted = false;
